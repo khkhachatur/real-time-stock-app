097c76017102be8c854aba625318b08f11f82a21
Khachatryan Khachatur <khachatryankhachatur57@gmail.com.com>
2025-11-05 12:16:00 +0100

    Implement Searc bar and bring lost files finhub.actions & utils

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import { NAV_ITEMS } from "@/lib/constants"
 import { usePathname } from "next/navigation"
+import SearchCommand from "./SearchCommand"
 
 
 const NavItem = () => {
@@ -17,11 +18,22 @@ const NavItem = () => {
 
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-       {NAV_ITEMS.map(({href, label})=>(
-        <li key={href}>
-            <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href) ? 'text-gray-100':''}`}>{label}</Link>
-        </li>
-       ))}
+       {NAV_ITEMS.map(({href, label}) => {
+          if(label ==="Search") return(
+            <li key='search-trigger'>
+              <SearchCommand
+              renderAs="text"
+              label="Search"
+              initialStocks={[]}
+              
+              />
+            </li>
+          )
+
+        return <li key={href}>
+                  <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href) ? 'text-gray-100':''}`}>{label}</Link>
+               </li>
+       })}
     </ul>
   )
 }
